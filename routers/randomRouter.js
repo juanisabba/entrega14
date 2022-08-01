bf67b3599199e0e5f53a10e169d8d5336e1612f4
juanisabba <68737695+juanisabba@users.noreply.github.com>
2022-08-01 12:20:24 -0300

    Add files via upload

---
+++ routers/randomRouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { fork } = require("child_process");
+const { Router } = express;
+const randomRouter = Router()
+
+
+module.exports = randomRouter;
+
+
+randomRouter.get('/', (req, res) => {
+    const numeros = req.query.cant || 100000000;
+    const forked = fork('../entrega14/randomNumbers.js', ['--c', numeros]);
+
+    forked.on('message', nums => {
+        //forked.send(numeros)
+        return res.json(nums)
+    })
+
+})
